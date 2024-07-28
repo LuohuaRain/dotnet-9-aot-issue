9e2a6a4cb4efcc0a195de867109c4a80e8b30507
LuohuaRain <37979368+LuohuaRain@users.noreply.github.com>
2024-07-28 10:03:15 +0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static partial class TestDllExport
+    {
+
+
+        [UnmanagedCallersOnly(CallConvs = [typeof(CallConvStdcall)], EntryPoint = nameof(Test))]
+        public static nint Test() => nint.Zero;
+
+        [UnmanagedCallersOnly(CallConvs = [typeof(CallConvStdcall)], EntryPoint = nameof(Test1))]
+        public static nint Test1() => nint.Zero;
+
+    }
+}
